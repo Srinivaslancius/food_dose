49413ce11cec5ca520d4707fc880cee8a445c277
Gunavardhan <vardhan.gujjula@gmail.com>
2017-12-05 11:59:02 +0530

    chnages committed

--- manage_webmaster/edit_lkp_states.php
+++ manage_webmaster/edit_lkp_states.php
@@ -31,10 +31,10 @@ $id = $_GET['stateid'];
                     <input type="text" name="state_name" class="form-control" id="user_input" placeholder="State Name" data-error="Please enter State Name" required value="<?php echo $getStatesData['state_name'];?>" onkeyup="checkUserAvailTest()">
                     <span id="input_status" style="color: red;"></span>
                     <input type="hidden" id="table_name" value="lkp_states">
-                    <input type="hidden" id="column_name" value="state_name"
+                    <input type="hidden" id="column_name" value="state_name">
                     <div class="help-block with-errors"></div>
                   </div>
-                 <?php $getStatus = getAllData('user_status');?>
+                 <?php $getStatus = getAllData('user_status');?> 
                   <div class="form-group">
                     <label for="form-control-3" class="control-label">Choose your status</label>
                     <select id="form-control-3" name="status" class="custom-select" data-error="This field is required." required>
